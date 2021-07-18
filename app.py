6b6ed6ff13e2af582a624d60b3f415a9345f1778
Yoshiaki Kobayashi <yoshiaki1973@gmail.com>
2021-07-19 00:50:29 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for
+import pathlib
+import gtts
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./mp3')
+#app = Flask(__name__)
 
 @app.route('/')
 def top():
     return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    a = request.form['text']
+    print(a)
+    tts = gtts.gTTS(a, lang="ja")
+    title = "sound.mp3"
+    tts.save( "./mp3/{0}".format(title))
+    return render_template('completed.html', file = title , text = a)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), threaded=True)
